d3054687023dea24460d1e4c1f1ce00ed5ed7771
douglas s dos anjos <douglasdsda@gmail.com>
2019-09-08 12:27:55 -0300

    versao inicial do projeto

---
+++ src/main/java/com/com/Lista2Es2Application.java
@@ -0,0 +1,13 @@
+package com.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista2Es2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lista2Es2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/com/Lista2Es2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lista2Es2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
